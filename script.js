8249dc70bf63f08dc00541742163b1ef6cba315d
athuls8991@gmail.com <athuls8991@gmail.com>
2023-08-09 23:18:09 +0530

    Add js file

---
+++ script.js
@@ -0,0 +1,31 @@
+function fnct(){
+    for(var i =0;i<16;i++){
+        document.getElementsByTagName('button')[i].addEventListener('click',clickFun);
+
+    }
+
+
+}
+
+fnct();
+var result ='';
+function clickFun(e){
+    e.preventDefault();
+    let inpt = document.getElementById('input');
+    let btnInp = e.target;
+
+    btnInp.classList.add('pressed');
+
+    setTimeout(()=>{
+        btnInp.classList.remove('pressed')
+
+    },90);
+
+    if(btnInp.textContent !="="){
+        inpt.value += btnInp.textContent;
+    }else{
+        result = inpt.value;
+        inpt.value = eval(result);
+    }
+
+}
